0d169f42beedf5e1cebb087b6f03f5b523fc5b0f
Chris Orem <corem@umich.edu>
2023-10-31 01:51:56 +0000

    Adding python script to combine files

---
+++ respondent_data_clean.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sys
+
+def clean_and_merge(contact_info_file, other_info_file, output_file):
+    contact_df = pd.read_csv(contact_info_file)
+    other_df = pd.read_csv(other_info_file)
+    merged_df = pd.merge(contact_df, other_df, on='respondent_id')
+    merged_df['birthdate'] = pd.to_datetime(merged_df['birthdate'], format='%m%d%Y').dt.strftime('%Y-%m-%d')
+    merged_df.to_csv(output_file, index=False)
+
+
+if __name__ == '__main__':
+    contact_info_file, other_info_file, output_file = sys.argv[1], sys.argv[2], sys.argv[3]
+    clean_and_merge(contact_info_file, other_info_file, output_file)
