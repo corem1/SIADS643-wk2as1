dc8366c7b34c75ee286bf5753ed79d25139d0c72
Chris Orem <corem@umich.edu>
2023-11-01 02:33:10 +0000

    Fixed birthdate data issue

--- respondent_data_clean.py
+++ respondent_data_clean.py
@@ -5,7 +5,13 @@ def clean_and_merge(contact_info_file, other_info_file, output_file):
     contact_df = pd.read_csv(contact_info_file)
     other_df = pd.read_csv(other_info_file)
     merged_df = pd.merge(contact_df, other_df, on='respondent_id')
-    merged_df['birthdate'] = pd.to_datetime(merged_df['birthdate'], format='%m%d%Y').dt.strftime('%Y-%m-%d')
+
+    def convert_birthdate(date_str):
+        date_str = str(date_str).zfill(8)
+        return pd.to_datetime(date_str, format='%m%d%Y').strftime('%Y-%m-%d')
+    
+    merged_df['birthdate'] = merged_df['birthdate'].apply(convert_birthdate)
+
     merged_df.to_csv(output_file, index=False)
 
 
